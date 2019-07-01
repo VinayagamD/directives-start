b866ca68476c6ff1f5224cbaac4e98f1f69adb4b
VinayagamD <vinayagam.d.ganesh@gmail.com>
2019-07-02 01:41:45 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import {BasicHighlightDirective} from './basic-highllight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/better-highlight/better-highlight.directive.spec.ts
@@ -0,0 +1,9 @@
+import { BetterHighlightDirective } from './better-highlight.directive';
+
+describe('BetterHighlightDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new BetterHighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
